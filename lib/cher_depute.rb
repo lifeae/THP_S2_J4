fca9390c8737066ba521f46fe209011c83d52eb5
lifeae <romain.leporatti@laposte.net>
2020-01-23 23:25:48 +0100

    last commit ...

--- lib/cher_depute.rb
+++ lib/cher_depute.rb
@@ -3,3 +3,23 @@ require 'nokogiri'
 
 page = Nokogiri::HTML(open("http://en.wikipedia.org/"))   
 puts page.class   # => Nokogiri::HTML::Document
+
+=begin
+Maintenant, fini de se faire mâcher le travail par tes gentils formateurs de THP. Tu dois récupérer la liste complète des députés de France ainsi que leurs adresses e-mail. Cherche par toi-même le site le plus aisé à scrapper et stocke les informations extraites dans une array de hashs selon ce format (un peu différent des exercices précédents) :
+
+a = [
+  { 
+    "first_name" => "Jean",
+    "last_name" => "Durant",
+    "email" => "jean.durant@assemblée.fr"
+  },
+  { 
+    "first_name" => "Martin",
+    "last_name" => "Dupont",
+    "email" => "martin.dupont@assemblée.fr"
+  },
+  etc
+]
+
+Pour les tests, nous t'invitons à te poser et t'inspirer des tests précédents. Deux tests suffiront.
+=end
--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -1,21 +1,75 @@
 require 'open-uri'
 require 'nokogiri'
 
-page = Nokogiri::HTML(open("http://en.wikipedia.org/"))   
-puts page.class   # => Nokogiri::HTML::Document
+def bonjour
+  url = "http://annuaire-des-mairies.com/val-d-oise.html"
+  puts "Ce programme va scrapper les emails des mairies du Val-d'Oise présentes sur le site : #{url}"
+end
+
+def scrap_townhalls
+  page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))   
+  puts "Récupération des noms des townhalls ..."
+  all_townhalls = page.xpath('//a[@class="lientxt"]')
+  puts "Fin de la récupération des noms des townhalls."
+  return all_townhalls
+end
+
+def townhalls_text(all_townhalls)
+  puts "Récupération du texte de chaque mairie et implémentation dans un nouveau tableau"
+  all_townhalls_text = []
+  all_townhalls.each do |townhall|
+    all_townhalls_text << townhall.text
+  end
+  all_townhalls_clean = all_townhalls_text
+  print all_townhalls_clean
+  return all_townhalls_clean
+end
+
+bonjour()
+townhalls_text(scrap_townhalls())
+
+
+def get_townhall_urls
+
+end
+
+def get_townhall_email(townhall_url)
+
+end
+
+get_townhall_email(get_townhall_urls)
+
 
 =begin
-a = [
-  { "ville_1" => "email_1" },
-  { "ville_2" => "email_2" }, 
-  etc
-]
+def scrap_emails
+  page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+  puts "Récupération des emails ..."
+  all_emails = page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]')
+  puts "Fin de la récupération des emails."
+  return all_emails
+end
 
-    Tu dois être capable d'obtenir un e-mail de mairie à partir de la page de cette dernière (pas en partant du listing complet des mairies). 
-    Fais d'abord une méthode get_townhall_email(townhall_url) qui récupère l’e-mail d'une mairie à partir de l'URL de mairies, par exemple celle de Avernes
-    Une fois que tu sais le faire pour une mairie, tu vas vouloir industrialiser et répéter ça sur tout l'annuaire du Val d'Oise. La prochaine étape est de coder une méthode get_townhall_urls qui récupère les URLs de chaque ville du Val d'Oise.
+def emails_text(all_emails)
+  puts "Récupération du texte constituant les emails et implémentation dans un nouveau tableau"
+  all_emails_text = [] 
+  all_emails.each do |email|
+    all_emails_text << email.text 
+  end
+  return all_emails_text
+end
 
-Quand tu es assez sûr que chaque méthode marche séparément, tu as juste à imbriquer les deux et à toi la gloire.
+def merge(all_townhalls_clean, all_emails_clean)
+  puts "Fusion des tableaux des townhalls et des emails"
+  hash = Hash[all_townhalls_clean.zip(all_emails_clean)] 
+  hash = hash.map {|townhall, email| {townhall => email}}
+  print hash
+  puts
+end
 
-Pour les tests, ce sera la même chose que pour l'exercice précédent mais appliqué aux deux méthodes : des tests de fonctionnement de base et des tests de cohérence du résultat.
+def perform
+  bonjour()
+  merge(townhalls_text(scrap_townhalls),emails_text(scrap_emails))
+end
+
+perform
 =end
