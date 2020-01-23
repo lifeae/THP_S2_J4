832e52c55e674937c7ce12f47719c74d03768550
lifeae <romain.leporatti@laposte.net>
2020-01-23 18:22:25 +0100

    add skeleton for tests files

--- spec/dark_trader_spec.rb
+++ spec/dark_trader_spec.rb
@@ -1,1 +1,37 @@
-require_relative
+require_relative '../lib/dark_trader'
+
+describe 'La méthode scrap_symbol' do
+  it 'Scrapper les symboles' do
+    expect(scrap_symbols()).to eq("")
+  end
+end
+
+describe 'La méthode scrap_prices' do
+  it 'Scrapper les prix' do
+    expect(scrap_prices()).to eq("")
+  end
+end
+
+describe 'La méthode symbols_text' do
+  it 'Récupérer le texte des symboles scrappés' do
+    expect(symbols_text()).to eq("")
+  end
+end
+
+describe 'La méthode prices_text' do
+  it 'Récupérer le texte des prix scrappés' do
+    expect(prices_text()).to eq("")
+  end
+end
+
+describe 'La méthode prices_without_dollars_tof' do
+  it 'Supprimer les : $ dans les prix et les passer en float' do
+    expect(prices_without_dollars_tof()).to eq("")
+  end
+end
+
+describe 'La méthode merge' do
+  it 'Fusionner les symboles et les prix' do
+    expect(merge(,)).to eq("")
+  end
+end
---
+++ spec/mairie_christmas_spec.rb
@@ -0,0 +1,37 @@
+require_relative '../lib/mairie_christmas'
+
+describe 'La méthode ' do
+  it '' do
+    expect(scrap_symbols()).to eq("")
+  end
+end
+
+describe 'La méthode ' do
+  it '' do
+    expect(scrap_prices()).to eq("")
+  end
+end
+
+describe 'La méthode ' do
+  it '' do
+    expect(symbols_text()).to eq("")
+  end
+end
+
+describe 'La méthode ' do
+  it '' do
+    expect(prices_text()).to eq("")
+  end
+end
+
+describe 'La méthode ' do
+  it '' do
+    expect(prices_without_dollars_tof()).to eq("")
+  end
+end
+
+describe 'La méthode ' do
+  it '' do
+    expect(merge(,)).to eq("")
+  end
+end
